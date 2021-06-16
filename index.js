1e031fc2631616b7320b42343344c3789b6c252f
Your Name <you@example.com>
2021-06-16 22:07:12 +0530

    changed port for heroku

--- index.js
+++ index.js
@@ -571,6 +571,6 @@ app.get('/user',(req,res)=>{
     });
 });
 
-app.listen(5000,()=>{
-    console.log("server is running on port 5000");
+app.listen(process.env.PORT || 5000,()=>{
+    // console.log("server is running on port 5000");
 });
